2d1f92349181096ad766d43cb09197b56dea63fb
QKeeper <moncer256710@gmail.com>
2026-09-03 11:17:10 +0500

    chore: save state before migrating from FSD to classic architecture

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,4 +1,4 @@
-const NotFound = () => {
+const NotFoundPage = () => {
   return (
     <div>
       <p>404 Page not found</p>
@@ -6,4 +6,4 @@ const NotFound = () => {
   );
 };
 
-export { NotFound };
+export { NotFoundPage };
