e256912f8e54de0cf85ba850352e70ed1208dcbb
QKeeper <moncer256710@gmail.com>
2026-09-12 01:30:28 +0500

    fix(backend): switch to standard @prisma/client generation and imports

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -2,7 +2,7 @@ import {
   PrismaClient,
   Prisma,
   NotificationType,
-} from "../generated/client/index.js";
+} from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import { env } from "../config/env.js";
